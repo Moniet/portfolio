c010ddee568416e3cbf197ef17ca1d6896c927cd
Moniet <moniet.sawhney@gmail.com>
2021-04-18 12:47:03 +0530

    📝 - button component

---
+++ src/components/design-system/Button/index.js
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled'
+import { themeGet } from '@styled-system/theme-get'
+import { layout, margin } from 'styled-system'
+
+const Button = styled.button`
+  ${layout}
+  ${margin}
+  min-width: 200px;
+  padding: ${themeGet('space.xs')};
+  background: ${themeGet('colors.text')};
+  color: ${themeGet('colors.bg')};
+  border: none;
+  border-radius: ${themeGet('radii.sm')};
+  outline: none;
+  cursor: pointer;
+  &:active {
+    transform: scale(0.98);
+    opacity: 0.95;
+  }
+  &:hover {
+    background: ${themeGet('colors.secondary')};
+  }
+
+  transition: opacity 0.2s ease, transform 0.2s ease;
+`
+export default Button
